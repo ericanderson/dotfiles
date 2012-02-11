3d61d14acf20c85f9c19114fa295f26daf1b2742
Eric Anderson <e@ericlanderson.com>
2012-02-10 18:11:50 -0800

    Adds and encrypts ssh/known_hosts and provides infrastructure for this.

--- README.md
+++ README.md
@@ -1,3 +1,22 @@
 # After fetching/pulling:
 1. Run `git submodule update`
 2. Followed by `./install.rb`
+3. Add ~/bin to your path
+
+# Decrypting certain files
+Definitely the following files are encrypted:
+* ssh/known_hosts
+* ssh/config
+
+To decrypt, do the above setup, then:
+
+1. Initialize the repo
+
+		$ gitcrypt init
+
+2. Enter the salt, password, and cipher from 1Password
+3. Invoke:
+
+		git reset --hard HEAD
+
+Props to [shadowhand](https://github.com/shadowhand/)'s [git-encrypt](https://github.com/shadowhand/git-encrypt) for this encrypted hotness.
--- install.rb
+++ install.rb
@@ -1,23 +1,41 @@
 #!/usr/bin/env ruby
-# from http://errtheblog.com/posts/89-huba-huba
 
 home = ENV['HOME']
 
 def symlinkable? file
   return true if File.extname(file).empty?
   return true if file == 'tmux.conf'
+  return true if file == 'bin'
+  return true if file == 'vim'
+  return false
 end
 
+# Regular dotfiles
 Dir.chdir File.dirname(__FILE__) do
-  dotfiles_dir = Dir.pwd.sub(home + '/', '')
+  cur_dir = Dir.pwd.sub(home + '/', '')
 
   Dir['*'].each do |file|
     next unless symlinkable? file
     target_name = file == 'bin' ? file : ".#{file}"
     target = File.join(home, target_name)
     unless File.exist? target
-      system %[ln -vsf #{File.join(dotfiles_dir, file)} #{target}]
+      system %[ln -vsf #{File.join(cur_dir, file)} #{target}]
     end
   end
 end
 
+# SSH files
+Dir.chdir File.join(File.dirname(__FILE__), "ssh") do
+  cur_dir = Dir.pwd.sub(home + '/', '')
+  
+  if (/ssh-rsa/ === File.read('known_hosts'))  
+    Dir['*'].each do |file|
+      target = File.join(home, ".ssh", file)
+      unless File.exist? target
+        system %[ln -vsf #{File.join(cur_dir, file)} #{target}]
+      end
+    end
+  else
+    puts "Skipping content for .ssh as the contents are still encrypted."
+  end # if
+end
