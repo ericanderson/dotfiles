24c0a59fd944973305d209e3871852b605baac5f
Eric Anderson <eanderson@palantir.com>
2016-12-05 16:32:51 -0500

    Install script no longer runs ./osx

--- install.sh
+++ install.sh
@@ -3,20 +3,18 @@
 set -e
 
 if [ ! -d "$HOME/.ssh" ]; then
-	ssh-keygen -t rsa
+	ssh-keygen -t rsa -b 4096
 	echo woot
 fi
 
 # Install Homebrew
 if [[ $OSTYPE == darwin* ]]; then
   if [ ! -d "/usr/local/Cellar" ]; then
-    ruby -e "$(curl -fsSL https://raw.github.com/Homebrew/homebrew/go/install)"
+    /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
   fi
 fi
 git submodule update --init
 ruby install.rb
-#brew bundle Brewfile
-bash ./osx
 
 if [[ $OSTYPE == darwin* ]]; then
   sh ~/.install/nodenv.sh
