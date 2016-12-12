a19bf5c0b56ccc5e19fbc02ec002a04333d8f0d0
Eric Anderson <eanderson@palantir.com>
2016-12-12 16:05:18 -0500

    Add hyper

---
+++ hyper_plugins/local/hyper-solarized-dark/index.js
@@ -0,0 +1,70 @@
+const backgroundColor = 'rgba(0, 43, 54, .5)';
+const foregroundColor = '#839496'
+const cursorColor = 'rgba(181, 137, 0, 0.6)'
+const borderColor = 'transparent'
+
+const colors = {
+  lightBlack:     '#002b36',
+  black:          '#073642',
+  lightGreen:     '#586e75',
+  lightYellow:    '#657b83',
+  lightBlue:      '#839496',
+  lightCyan:      '#93a1a1',
+  white:          '#eee8d5',
+  lightWhite:     '#fdf6e3',
+  yellow:         '#b58900',
+  lightRed:       '#cb4b16',
+  red:            '#dc322f',
+  magenta:        '#d33682',
+  lightMagenta:   '#6c71c4',
+  blue:           '#268bd2',
+  cyan:           '#2aa198',
+  green:          '#859900'
+}
+
+module.exports.onWindow = browserWindow => browserWindow.setVibrancy('dark');
+
+exports.decorateConfig = config => {
+
+  return Object.assign({}, config, {
+    foregroundColor,
+    backgroundColor,
+    borderColor,
+    cursorColor,
+    colors,
+    termCSS: `
+      ${config.termCSS || ''}
+    `,
+    css: `
+      ${config.css || ''}
+      * {
+      	-webkit-font-feature-settings: "liga" on, "calt" on, "dlig" on !important;
+      	text-rendering: optimizeLegibility !important;
+        font-weight: 500;
+      }
+      .tabs_list {
+      	border: 0;
+      }
+      .tabs_nav {
+      	background-color: #001f27 !important;
+      }
+      .tab_tab {
+        color: ${foregroundColor} !important;
+        background-color: #001f27;
+				border-color: ${borderColor} !important;
+      }
+      .tab_tab:before {
+      	border: 0;
+      }
+      .tab_tab.tab_active {
+        border: transparent !important;
+        font-weight: bold;
+        color: #b3b3b3 !important;
+        background-color: ${backgroundColor};
+      }
+      .splitpane_divider {
+      	background-color: #001f27 !important;	
+      }
+    `
+  })
+}
---
+++ hyper_plugins/local/hyper-solarized-dark/readme.md
@@ -0,0 +1,25 @@
+#Solarized Dark theme for Hyper
+
+A port of the [Solarized Dark](http://ethanschoonover.com/solarized) theme for [Hyper.app](https://hyper.is)
+
+![Solarized Dark Hyper](https://dl.dropboxusercontent.com/u/29130579/hyperterm-solarized-dark.png)
+
+### Installation
+1. Open Hyper's preferences by pressing `Cmd + ,`.
+2. Find and Update the plugins array to include `hyper-solarized-dark`:  
+
+  ```js
+  plugins: [  
+    'hyper-solarized-dark'  
+  ],
+  ```
+
+3. Reload Hyper by pressing `Cmd + Shft + R`
+
+
+### Related
+- [Solarized Light Theme](https://github.com/Ghosh/hyper-solarized-dark)
+
+```
+License - MIT
+```
