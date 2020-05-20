f38a97e7510253ae0a0ba0a7fafdce76c64c66a0
Eric Anderson <eanderson@palantir.com>
2020-05-20 11:09:07 -0400

    Better up arrow support for zsh

---
+++ zsh_custom/plugins/sane-history/sane-history.plugin.zsh
@@ -0,0 +1,18 @@
+bindkey "${key[Up]}" up-line-or-local-history
+bindkey "${key[Down]}" down-line-or-local-history
+
+up-line-or-local-history() {
+    zle set-local-history 1
+    zle up-line-or-history
+    zle set-local-history 0
+}
+zle -N up-line-or-local-history
+down-line-or-local-history() {
+    zle set-local-history 1
+    zle down-line-or-history
+    zle set-local-history 0
+}
+zle -N down-line-or-local-history
+
+bindkey "^[[1;5A" up-line-or-history    # [CTRL] + Cursor up
+bindkey "^[[1;5B" down-line-or-history  # [CTRL] + Cursor down
