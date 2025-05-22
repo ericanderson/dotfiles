c3479975e4cdf464c3ecb59ab89f73b2bc30a514
Eric Anderson <e@ericlanderson.com>
2025-05-22 21:39:19 +0000

    minor improvements for devcontainer

--- run_once_install-packages-pip3.sh
+++ run_once_install-packages-pip3.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
-pip3 install --user pynvim
+# Check if pip3 is available
+if command -v pip3 &> /dev/null; then
+    pip3 install --user pynvim
+else
+    echo "pip3 not found, skipping pynvim installation"
+fi
